a86d84f86536badc89d07ce234335983985be466
Derek Shoemaker <shoemakerdr@gmail.com>
2017-05-08 13:16:21 -0500

    deleted trailing whitespace

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 const WikiApp = (function () {
 
   document.addEventListener("DOMContentLoaded", function() {
-  
+
     const input = document.getElementById("input");
     const results = document.getElementById("results");
     // all buttons except for first which is link to random article
     const buttons = Array.prototype.slice.apply(document.getElementsByTagName("button")).slice(1);
     let tempscript = null;
     let timeout;
-    
+
     function startQuery() {
       if (input.value.length === 0)
         clearButtons();
       clearTimeout(timeout);
       timeout = setTimeout(startFetch, 250);
     }
-    
+
     function startFetch() {
       tempscript = document.createElement("script");
       tempscript.src = "http://en.wikipedia.org/w/api.php"
@@ -26,14 +26,14 @@ const WikiApp = (function () {
       document.body.appendChild(tempscript);
       // onFetchComplete invoked when finished through JSONP
     }
-    
+
     // onFetchComplete must be global to be used as JSONP callback
     window.onFetchComplete = function onFetchComp(data) {
       renderButtons(data);
       document.body.removeChild(tempscript);
       tempscript = null;
     };
-    
+
     // clears button node
     function clearButtons() {
       buttons.forEach(button => {
@@ -42,7 +42,7 @@ const WikiApp = (function () {
       });
       results.style.display = "none";
     }
-    
+
     function renderButtons(data) {
       buttons.forEach((button, i) => {
         if (!data[1][i])
@@ -52,7 +52,7 @@ const WikiApp = (function () {
           const wrapper = document.createElement("div");
           const title = document.createElement("h3");
           const description = document.createElement("p");
-          
+
           title.textContent = data[1][i];
           description.textContent = data[2][i];
           wrapper.appendChild(title);
@@ -62,12 +62,12 @@ const WikiApp = (function () {
           button.onclick = function() { window.open(data[3][i], "_blank"); };
         }
       });
-      if (results.style.display === "none") 
+      if (results.style.display === "none")
         results.style.display = "";
     }
-    
+
     input.addEventListener("input", startQuery);
-  
+
   });
 
 })();
